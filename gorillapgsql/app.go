9b4ceb2472076c4eded3e8a477c0529c50cba1d5
Sivanesh Logandurai <sivanesh.l@gmail.com>
2019-01-30 00:58:18 +0530

    starting with tests

--- gorillapgsql/app.go
+++ gorillapgsql/app.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gorilla/mux"
 )
 
+// App ...
 type App struct {
 	Router *mux.Router
 	DB     *sql.DB
 }
 
+// Initialize ...
 func (a *App) Initialize(user, password, dbname string) {
+
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
 
 }
 
+// Run ...
 func (a *App) Run(addr string) {
 
 }
--- gorillapgsql/main_test.go
+++ gorillapgsql/main_test.go
@@ -1,20 +1,88 @@
-package main_test
+package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-var a main.App
+var a App
 
 func TestMain(m *testing.M) {
-	a = main.App{}
+	a = App{}
 
-	a.Initialize("admin", "admin", "testdb")
+	a.Initialize("postgres", "admin", "testdb")
 
 	ensureTableExists()
 
+	code := m.Run()
+
+	clearTable()
+
+	os.Exit(code)
+
+}
+
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an Empty array. Got %s", body)
+	}
+
+}
+
+func TestGetNonExistentProduct(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET,", "/product/11", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "Product not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got %s", m["error"])
+	}
 }
 
 func ensureTableExists() {
-
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
 }
+
+func clearTable() {
+	a.DB.Exec("DELETE * FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected respomse code %d. Got %d", expected, actual)
+	}
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+id SERIAL,
+name TEXT NOT NULL,
+price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
