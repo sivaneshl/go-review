1fa20c80ab7afa10a80920e9d37f05b27d94841d
Sivanesh Logandurai <sivanesh.l@gmail.com>
2019-01-30 01:22:51 +0530

    TestCreateProduct, TestGetProduct

--- gorillapgsql/main_test.go
+++ gorillapgsql/main_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -43,7 +45,7 @@ func TestEmptyTable(t *testing.T) {
 func TestGetNonExistentProduct(t *testing.T) {
 	clearTable()
 
-	req, _ := http.NewRequest("GET,", "/product/11", nil)
+	req, _ := http.NewRequest("GET", "/product/11", nil)
 	response := executeRequest(req)
 
 	checkResponseCode(t, http.StatusNotFound, response.Code)
@@ -53,6 +55,41 @@ func TestGetNonExistentProduct(t *testing.T) {
 	if m["error"] != "Product not found" {
 		t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got %s", m["error"])
 	}
+}
+
+func TestCreateProduct(t *testing.T) {
+	clearTable()
+
+	payload := []byte(`{"name":"test_product","price":74.84}`)
+	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(payload))
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test_product" {
+		t.Errorf("Expected the product name to be 'test_product'. Got %v", m["name"])
+	}
+
+	if m["price"] != 74.84 {
+		t.Errorf("Expected the price to be '74.84'. Got %v", m["price"])
+	}
+
+	if m["id"] != 1.0 {
+		t.Errorf("Expected the id to be '1.0'. Got %v", m["id"])
+	}
+}
+
+func TestGetProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
 }
 
 func ensureTableExists() {
@@ -76,6 +113,16 @@ func executeRequest(req *http.Request) *httptest.ResponseRecorder {
 func checkResponseCode(t *testing.T, expected, actual int) {
 	if expected != actual {
 		t.Errorf("Expected respomse code %d. Got %d", expected, actual)
+	}
+}
+
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name,price) VALUES($1,$2)", "Product_"+strconv.Itoa(i), (i+1.0)*10)
 	}
 }
 
